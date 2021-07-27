f4a2f3eb005b65416c32880ba5875885e8d22d92
iiskakov <iiskakov@student.42.us.org>
2021-07-27 16:00:19 +0600

    Basic Turing Machine Done?😯

--- src/ft_turing/core.clj
+++ src/ft_turing/core.clj
@@ -11,29 +11,71 @@
                   :key-fn keyword))
     )
 
+  (def idx (atom 0))
+  (def tape (atom ["1" "0"]))
+
   (load-description)
-  (get parsed :blank)
+  (get parsed :transitions)
+  (reset! tape [(get parsed :blank)])
 
-  (def sm {:subone [{:read '1 :write '0 :action 'action1 :to_state 'subtwo}
-                    {:read '- :write '2 :action 'action2 :to_state 'subtwo}]
-           :subtwo [{:read '0 :write '1 :action 'action3 :to_state 'subone}
-                    {:read '- :write '2 :action 'action4 :to_state 'subtwo}]})
+  (def sm {:subone [{:read "1" :write "0" :action "RIGHT" :to_state "subtwo"}
+                    {:read "-" :write "2" :action "RIGHT" :to_state "subtwo"}]
+           :subtwo [{:read "0" :write "1" :action "RIGHT" :to_state "subone"}
+                    {:read "-" :write "2" :action "RIGHT" :to_state "subtwo"}]})
 
 
-  (defn compute [input symbol]
+
+  ;; Write/Read Functions
+  (defn insert-start [target addition]
+    (apply conj (if (vector? addition) addition [addition]) target))
+
+  (defn insert-end [target addition]
+    (conj target addition))
+
+  (defn write-left [symbol]
+    (cond
+      (= @idx 0) (swap! tape insert-start symbol)
+      :otherwise
+        (when true
+          (swap! idx dec)
+          (swap! tape assoc @idx symbol)
+          )))
+
+  (defn write-right [symbol]
+    (cond
+      (= @idx (count @tape))
+        (when true
+          (swap! tape insert-end symbol)
+          (swap! idx inc))
+      :otherwise
+        (when true
+          (swap! idx inc)
+          (swap! tape assoc @idx symbol)
+          )))
+
+  (defn write-tape [direction symbol]
+    (cond
+      (= direction "LEFT") (write-left symbol)
+      (= direction "RIGHT") (write-right symbol))
+    (println @tape)
+    (flush))
+;; rewrite with if else instead of cond
+
+  (defn read-tape []
+    (nth @tape @idx))
+
+  (reset! idx 0)
+  ;;can probably start with (transition initial)
+
+  (defn compute [input]
     (some (fn [{:keys [read action to_state write]}]
-        (when (= read symbol)
-          (println write)
+        (when (= read (read-tape))
+          (write-tape action write)
           (transition to_state)))
                             input))
 
   (defn transition [state]
-   (compute (get sm (keyword state)) '1))
+   (compute (get sm (keyword state))))
 
-  ;; TODO: implement tape and movements (left, right + write)
-  ;;
-  ;;
-  ;;
-  ;;can probably start with (transition initial)
   )
 
